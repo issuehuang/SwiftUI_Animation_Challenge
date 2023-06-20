547b3234f7cdec6342f44051677dd581b246dc11
Victor Huang <iosdeveloperforwork@gmail.com>
2023-06-20 16:00:53 +0800

    Initial Commit

---
+++ Animation Challenge/Animation_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animation_ChallengeApp.swift
+//  Animation Challenge
+//
+//  Created by Victor on 2023/6/20.
+//
+
+import SwiftUI
+
+@main
+struct Animation_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

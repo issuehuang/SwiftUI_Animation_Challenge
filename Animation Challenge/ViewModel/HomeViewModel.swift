9052300c503cb464ccbda7984c2ab3e094f0202d
Victor Huang <iosdeveloperforwork@gmail.com>
2023-06-26 11:00:46 +0800

    add images

---
+++ Animation Challenge/ViewModel/HomeViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewModel.swift
+//  Animation Challenge
+//
+//  Created by Victor on 2023/6/20.
+//
+
+import SwiftUI
+
+struct HomeViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeViewModel()
+    }
+}

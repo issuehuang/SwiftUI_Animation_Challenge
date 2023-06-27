450a2974aeeb5a6c4a7f59580512df8aba4bc4a1
Victor Huang <iosdeveloperforwork@gmail.com>
2023-06-27 11:54:56 +0800

    add CustomCorners file

---
+++ Animation Challenge/View/CustomCorners.swift
@@ -0,0 +1,20 @@
+//
+//  CustomCorners.swift
+//  Animation Challenge
+//
+//  Created by Victor on 2023/6/27.
+//
+
+import SwiftUI
+
+struct CustomCorners: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomCorners_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomCorners()
+    }
+}
